849546bd96071cf8d3580e50402fab02667632bf
Artjosh <arthur.heleus.thade@gmail.com>
2025-05-01 03:03:43 -0300

    vai vercel

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import admin from "@/lib/firebase-admin"
 import { createToken } from "@/lib/jwt"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 export async function POST(request) {
   try {
